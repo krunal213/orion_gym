520e37e4306e621e96ec2b6006033aff72798f1d
thebhangarwale devapps <thebhangarwaledevapps@gmail.com>
2023-01-19 21:12:58 +0530

    Added Test

--- app/src/test/java/com/app/orion/ExampleUnitTest.kt
+++ app/src/test/java/com/app/orion/ExampleUnitTest.kt
@@ -10,11 +10,6 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-
 
     @Test
     fun or_operator_test() {
